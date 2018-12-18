d64ece8d29d8ea9ea345100523614001a4463070
Luyanda Hatta <44491839+LuyandaHatta@users.noreply.github.com>
2018-12-18 09:36:06 +0200

    Initial commit

---
+++ Luyanda's website/main.js
@@ -0,0 +1,8 @@
+﻿function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(-26.172455, 28.127398),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
